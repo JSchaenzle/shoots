19b33936e21ab7039ad410a6641996a9940ac093
Jordan Schaenzle <jschaenzle@gmail.com>
2016-09-20 11:39:37 -0400

    Moved webRequestAction to it's own file

--- src/actions/photoshootActions.js
+++ src/actions/photoshootActions.js
@@ -1,6 +1,3 @@
-import fetch from 'isomorphic-fetch';
-import $ from 'jquery';
-import { browserHistory } from 'react-router';
 import {
   ADD_PHOTOSHOOT_START,
   ADD_PHOTOSHOOT_SUCCESS,
@@ -15,6 +12,7 @@ import {
   DELETE_PHOTOSHOOT_SUCCESS,
   DELETE_PHOTOSHOOT_ERROR
 } from './actionTypes.js';
+import { webRequestAction } from './webRequestAction.js';
 
 const addPhotoshootStarted = () => {
   return {
@@ -120,43 +118,6 @@ const retrieveAllPhotoshootsError = (errorInfo) => {
   };
 };
 
-function webRequestAction(url, config) {
-  return (dispatch, getState) => {
-    dispatch(config.preRequest());
-
-    const user = getState().accounts.activeSession.user;
-    const authToken = user ? user.auth_token : null;
-    let headers = {
-      SHOOTS_AUTH_TOKEN: authToken
-    };
-    console.log("Headers: ", headers);
-
-    return $.ajax(url, {
-      method: config.method,
-      headers: headers,
-      data: config.data,
-      statusCode: {
-        401: (xhr) => {
-          console.log("Redirecting to login due to unauthorized");
-          browserHistory.push("/account-access");
-        }
-      }
-    })
-      .then(
-        (response) => {
-          let processFunc = config.processResponseData;
-          let result = null;
-          if (processFunc) {
-            result = processFunc(response);
-          }
-          dispatch(config.onSuccess(result));
-        },
-        (xhr, status, error) => {
-          dispatch(config.onError(error));
-          console.log("Error processing api request: ", url, status, error);
-        });
-  };
-}
 
 export function requestRetrieveAllPhotoshoots(details) {
   return webRequestAction(`/api/photoshoots`, {
---
+++ src/actions/webRequestAction.js
@@ -0,0 +1,39 @@
+import $ from 'jquery';
+
+export const webRequestAction = (url, config) => {
+  return (dispatch, getState) => {
+    dispatch(config.preRequest());
+
+    const user = getState().accounts.activeSession.user;
+    const authToken = user ? user.auth_token : null;
+    let headers = {
+      SHOOTS_AUTH_TOKEN: authToken
+    };
+    console.log("Headers: ", headers);
+
+    return $.ajax(url, {
+      method: config.method,
+      headers: headers,
+      data: config.data,
+      statusCode: {
+        401: (xhr) => {
+          console.log("Redirecting to login due to unauthorized");
+          browserHistory.push("/account-access");
+        }
+      }
+    })
+      .then(
+        (response) => {
+          let processFunc = config.processResponseData;
+          let result = null;
+          if (processFunc) {
+            result = processFunc(response);
+          }
+          dispatch(config.onSuccess(result));
+        },
+        (xhr, status, error) => {
+          dispatch(config.onError(error));
+          console.log("Error processing api request: ", url, status, error);
+        });
+  };
+}
